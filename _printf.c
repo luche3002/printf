dd2a40c8c1cde9498c3cdd26bf24aa9e11ad9616
Shighi <daisyshighi@gmail.com>
2024-07-29 04:10:47 -0700

    task 0 FC

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <unistd.h>
-#include <stdlib.h>
-#define BUFF_SIZE 1024
+#include <stdarg.h>
+
 /**
  * _printf - Custom printf function
  * @format: Format string
@@ -9,387 +9,80 @@
  */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-if (format == NULL)
-return (-1);
-va_start(list, format);
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-printed_chars++;
+	va_list args;
+	int i = 0, count = 0;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					count += print_char(args);
+					break;
+				case 's':
+					count += print_string(args);
+					break;
+				case '%':
+					count += print_percent();
+					break;
+				default:
+					write(1, &format[i - 1], 1);
+					write(1, &format[i], 1);
+					count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
 }
-else
-{
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
-}
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
+
 /**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * print_char - Prints a character
+ * @args: Argument list
+ * Return: Number of characters printed
  */
-void print_buffer(char buffer[], int *buff_ind)
+int print_char(va_list args)
 {
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-*buff_ind = 0;
+	char c = va_arg(args, int);
+
+	return (write(1, &c, 1));
 }
-/**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
- */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-int flags, int width, int precision, int size)
-{
-int i, unknow_len = 0, printed_chars = -1;
-fmt_t fmt_types[] = {
-{'c', print_char}, {'s', print_string}, {'%', print_percent},
-{'i', print_int}, {'d', print_int}, {'b', print_binary},
-{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
-};
-for (i = 0; fmt_types[i].fmt != '\0'; i++)
-if (fmt[*ind] == fmt_types[i].fmt)
-return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-if (fmt_types[i].fmt == '\0')
-{
-if (fmt[*ind] == '\0')
-return (-1);
-unknow_len += write(1, "%%", 1);
-if (fmt[*ind - 1] == ' ')
-unknow_len += write(1, " ", 1);
-else if (width)
-{
---(*ind);
-while (fmt[*ind] != ' ' && fmt[*ind] != '%')
---(*ind);
-if (fmt[*ind] == ' ')
---(*ind);
-return (1);
-}
-unknow_len += write(1, &fmt[*ind], 1);
-return (unknow_len);
-}
-return (printed_chars);
-}
-/* Functions to print chars and strings */
-/**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags: Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
-int print_char(va_list types, char buffer[],
-int flags, int width, int precision, int size)
-{
-char c = va_arg(types, int);
-return (handle_write_char(c, buffer, flags, width, precision, size));
-}
+
 /**
  * print_string - Prints a string
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * @args: Argument list
+ * Return: Number of characters printed
  */
-int print_string(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+int print_string(va_list args)
 {
-int length = 0, i;
-char *str = va_arg(types, char *);
-UNUSED(buffer);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
-if (str == NULL)
-{
-str = "(null)";
-if (precision >= 6)
-str = "      ";
+	char *str = va_arg(args, char *);
+	int len = 0;
+
+	if (str == NULL)
+		str = "(null)";
+	while (str[len])
+		len++;
+
+	return (write(1, str, len));
 }
-while (str[length] != '\0')
-length++;
-if (precision >= 0 && precision < length)
-length = precision;
-if (width > length)
-{
-if (flags & F_MINUS)
-{
-write(1, &str[0], length);
-for (i = width - length; i > 0; i--)
-write(1, " ", 1);
-return (width);
-}
-else
-{
-for (i = width - length; i > 0; i--)
-write(1, " ", 1);
-write(1, &str[0], length);
-return (width);
-}
-}
-return (write(1, str, length));
-}
-/* Functions to print numbers */
+
 /**
- * print_int - Print int
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_percent - Prints a percent sign
+ * Return: Number of characters printed
  */
-int print_int(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+int print_percent(void)
 {
-int i = BUFF_SIZE - 2;
-int is_negative = 0;
-long int n = va_arg(types, long int);
-unsigned long int num;
-n = convert_size_number(n, size);
-if (n == 0)
-buffer[i--] = '0';
-buffer[BUFF_SIZE - 1] = '\0';
-num = (unsigned long int)n;
-if (n < 0)
-{
-num = (unsigned long int)((-1) * n);
-is_negative = 1;
-}
-while (num > 0)
-{
-buffer[i--] = (num % 10) + '0';
-num /= 10;
-}
-i++;
-return (write_number(is_negative, i, buffer, flags, width, precision, size));
-}
-/**
- * print_binary - Prints an unsigned number
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Numbers of char printed.
- */
-int print_binary(va_list types, char buffer[],
-int flags, int width, int precision, int size)
-{
-unsigned int n, m, i, sum;
-unsigned int a[32];
-int count;
-UNUSED(buffer);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
-n = va_arg(types, unsigned int);
-m = 2147483648; /* (2 ^ 31) */
-a[0] = n / m;
-for (i = 1; i < 32; i++)
-{
-m /= 2;
-a[i] = (n / m) % 2;
-}
-for (i = 0, sum = 0, count = 0; i < 32; i++)
-{
-sum += a[i];
-if (sum || i == 31)
-{
-char z = '0' + a[i];
-write(1, &z, 1);
-count++;
-}
-}
-return (count);
-}
-/* Function to print non printable characters */
-/**
- * print_non_printable - Prints ascii codes in hexa of non printable chars
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags: Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
- */
-int print_non_printable(va_list types, char buffer[],
-int flags, int width, int precision, int size)
-{
-int i = 0, offset = 0;
-char *str = va_arg(types, char *);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
-if (str == NULL)
-return (write(1, "(null)", 6));
-while (str[i] != '\0')
-{
-if (is_printable(str[i]))
-buffer[i + offset] = str[i];
-else
-offset += append_hexa_code(str[i], buffer, i + offset);
-i++;
-}
-buffer[i + offset] = '\0';
-return (write(1, buffer, i + offset));
-}
-/* Other functions */
-/**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
- */
-int get_flags(const char *format, int *i)
-{
-/* - + 0 # ' ' */
-/* 1 2 4 8  16 */
-int j, curr_i;
-int flags = 0;
-const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
-for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-{
-for (j = 0; FLAGS_CH[j] != '\0'; j++)
-if (format[curr_i] == FLAGS_CH[j])
-{
-flags |= FLAGS_ARR[j];
-break;
-}
-if (FLAGS_CH[j] == 0)
-break;
-}
-*i = curr_i - 1;
-return (flags);
-}
-/**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: width.
- */
-int get_width(const char *format, int *i, va_list list)
-{
-int curr_i;
-int width = 0;
-for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-{
-if (is_digit(format[curr_i]))
-{
-width *= 10;
-width += format[curr_i] - '0';
-}
-else if (format[curr_i] == '*')
-{
-curr_i++;
-width = va_arg(list, int);
-break;
-}
-else
-break;
-}
-*i = curr_i - 1;
-return (width);
-}
-/**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
- */
-int get_precision(const char *format, int *i, va_list list)
-{
-int curr_i = *i + 1;
-int precision = -1;
-if (format[curr_i] != '.')
-return (precision);
-precision = 0;
-for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-{
-if (is_digit(format[curr_i]))
-{
-precision *= 10;
-precision += format[curr_i] - '0';
-}
-else if (format[curr_i] == '*')
-{
-curr_i++;
-precision = va_arg(list, int);
-break;
-}
-else
-break;
-}
-*i = curr_i - 1;
-return (precision);
-}
-/**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
- * Return: Size.
- */
-int get_size(const char *format, int *i)
-{
-int curr_i = *i + 1;
-int size = 0;
-if (format[curr_i] == 'l')
-size = S_LONG;
-else if (format[curr_i] == 'h')
-size = S_SHORT;
-if (size == 0)
-*i = curr_i - 1;
-else
-*i = curr_i;
-return (size);
+	return (write(1, "%", 1));
 }
